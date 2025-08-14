cfe601f77800e274d1fd76f47bd2a11a1da26ce5
Breno Leal <brenolealba@gmail.com>
2025-08-14 16:20:52 -0300

    terceiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '20px' }}>
-      <h1>SMSJUR</h1>
-      <p>App de Recepção de Demandas - Next.js</p>
-      <p>Deploy configurado para uso gratuito na Vercel.</p>
-    </div>
-  );
+import Home from '../app/page.jsx';
+
+export default function Index() {
+  return <Home />;
 }
